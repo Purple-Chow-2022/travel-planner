9fe6a32b24f01af1afc5a28ca3dd3828f081f232
purple.chow <purple.chow@toppansecurity.com>
2026-01-11 22:12:25 +0800

    deployment update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/travel-planner/', // This MUST match your GitHub repo name exactly
   plugins: [react()],
 })
